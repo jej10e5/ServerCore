7bf5d17f152b85647399b1d3bc7dee5d119e5e4d
jej <jej@DESKTOP-09GUFT2>
2026-06-18 00:09:10 +0900

    기본적인 소켓 연결 구현

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -7,102 +7,83 @@
 #include <future>
 #include "ThreadManager.h"
 
-#include "RefCounting.h"
-#include "Memory.h"
-#include "Allocator.h"
+#include <WinSock2.h>
+#include <mswsock.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
 
-using TL = TypeList<class Player, class Mage, class Knight, class Archer>;
-
-class Player
-{
-public:
-	Player()
-	{
-		INIT_TL(Player);
-	}
-	virtual ~Player() { }
-
-	DECLARE_TL
-};
-
-class Knight : public Player
-{
-public:
-	Knight() { INIT_TL(Knight); }
-};
-
-class Mage : public Player
-{
-
-public:
-	Mage() { INIT_TL(Mage); }
-};
-
-class Archer : public Player
-{
-
-public:
-	Archer() { INIT_TL(Archer) }
-};
-
-class Dog
-{
-
-};
 
 int main()
 {
-	//TypeList<Mage, Knight>::Head whoAMI;
-	//TypeList<Mage, Knight>::Tail whoAMI2;
+	// 윈속 초기화(ws2_32 라이브러리 초기화)
+	// 관련 정보가 wsaData에 채워짐
+	WSAData wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
 
-	//TypeList<Mage, TypeList<Knight, Archer>>::Head whoAMI3;
-	//TypeList<Mage, TypeList<Knight, Archer>>::Tail::Head whoAMI4;
-	//TypeList<Mage, TypeList<Knight, Archer>>::Tail::Tail whoAMI5;
-
-	//int32 len1 = Length<TypeList<Mage, Knight>>::value;
-	//int32 len2 = Length<TypeList<Mage, Knight, Archer>>::value;
-	//
-	//
-	//// 3*3 
-	//TypeAt<TL, 0>::Result whoAMI6;
-	//TypeAt<TL, 1>::Result whoAMI7;
-	//TypeAt<TL, 2>::Result whoAMI8;
-
-	//int32 index1 = IndexOf<TL, Mage>::value;
-	//int32 index2 = IndexOf<TL, Archer>::value;
-	//int32 index3 = IndexOf<TL, Dog>::value;
-
-	//bool canConvert1 = Conversion<Player, Knight>::exists;
-	//bool canConvert2 = Conversion<Knight, Player>::exists;
-	//bool canConvert3 = Conversion<Knight, Dog>::exists;
-
-	/*{
-		Player* player = new Knight();
-
-		bool canCast = CanCast<Knight*>(player);
-		Knight* knight = TypeCast<Knight*>(player);
-		
-
-		delete player;
-	}*/
-
+	// 소켓 만들기
+	// ad : Address FAmily (AF_INET = IPv4, AF_INET6 = IPv6)
+	// type : TCP(SOCK_STREAM) vs UDP(SOCK_DGRAM)
+	// protocol : 0
+	// return : desciptor
+	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0); // 문지기 - 처음에 대표번호로 사용하는 소켓
+	if (listenSocket == INVALID_SOCKET)
 	{
-		shared_ptr<Player> player = MakeShared<Knight>();
-
-		shared_ptr<Archer> archer = TypeCast<Archer>(player);
-		bool canCast = CanCast<Mage>(player);
-
+		int32 errCode = ::WSAGetLastError();
+		cout << "Socket ErrorCode : " << errCode << endl;
+		return 0;
 	}
+
+	// 나의 주소는 ? (IP주소 + Port) -> XX 아파트 YY 호
+	SOCKADDR_IN serverAddr; //IPv4
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY); // 알아서 해줘 - 알아서 나를 의미하는 모든 주소로 설정
+	serverAddr.sin_port = ::htons(7777); // 연결할때 사용할 포트
+
+	// 안내 폰 개통! 식당의 대표 번호
+	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Bind ErrorCode : " << errCode << endl;
+		return 0;
+	}
+
+	// 영업 시작
+	if (::listen(listenSocket, 10) == SOCKET_ERROR)
+	{
+		int32 errCode = ::WSAGetLastError();
+		cout << "Listen ErrorCode : " << errCode << endl;
+		return 0;
+	}
+
+	//------------------------------------
 	
-	for (int32 i = 0; i < 5; i++)
+	while (true)
 	{
-		GThreadManager->Launch([]()
-			{
-				while (true)
-				{
-				}
-			});
+		SOCKADDR_IN clientAddr; //IPv4
+		::memset(&clientAddr, 0, sizeof(clientAddr));
+		int32 addrLen = sizeof(clientAddr);
+		
+		// 실질적으로 클라이언트랑 통신하는 단말기
+		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+		if (clientSocket == INVALID_SOCKET)
+		{
+			int32 errCode = ::WSAGetLastError();
+			cout << "Accept ErrorCode : " << errCode << endl;
+			return 0;
+		}
+
+		// 손님 입장
+		char ipAddress[16];
+		::inet_ntop(AF_INET, &clientAddr.sin_addr, ipAddress, sizeof(ipAddress));
+		cout << "Client Connected! IP = " << ipAddress << endl;
+
+		// TODO
 	}
 
-	GThreadManager->Join();
+	//------------------------------------
+
+	// 윈속 종료
+	::WSACleanup();
 }
