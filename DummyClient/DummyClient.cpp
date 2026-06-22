d45a83be254a2ba764cfae55a51f658c6a20f87f
jej <jej@DESKTOP-09GUFT2>
2026-06-22 23:48:16 +0900

    논블로킹 소켓

--- DummyClient/DummyClient.cpp
+++ DummyClient/DummyClient.cpp
@@ -25,6 +25,79 @@ int main()
 		HandleError("Socket");
 	}
 
+	u_long on = 1;
+	::ioctlsocket(clientSocket, FIONBIO, &on);
+
+	SOCKADDR_IN serverAddr;
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+	serverAddr.sin_port = ::htons(7777);
+
+	//Connet
+	while (true)
+	{
+		if(::connect(clientSocket, (SOCKADDR*) & serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+		{
+			// 원래 블록했어야했는데,,, 너가 논블로킹으로 하라며??
+			if (::WSAGetLastError() == WSAEWOULDBLOCK)
+				continue;
+
+			// 이미 연결된 상태라면 break;
+			if (::WSAGetLastError() == WSAEISCONN)
+				break;
+
+			// Error
+			break;
+		}
+	}
+
+	cout << "Conneted to Server!" << endl;
+
+	char sendBuffer[100] = "Hello World";
+
+	//Send
+	while (true)
+	{
+		if (::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0) == SOCKET_ERROR)
+		{
+			// 원래 블록했어야했는데,,, 너가 논블로킹으로 하라며??
+			if (::WSAGetLastError() == WSAEWOULDBLOCK)
+				continue;
+
+			// Error
+			break;
+		}
+
+		cout << "Send Data! Len = " << sizeof(sendBuffer) << endl;
+
+		while (true)
+		{
+			char recvBuffer[1000];
+			int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+			if (recvLen == SOCKET_ERROR)
+			{
+				// 원래 블록했어야했는데,,, 너가 논블로킹으로 하라며??
+				if (::WSAGetLastError() == WSAEWOULDBLOCK)
+					continue;
+
+				// Error
+				break;
+			}
+			else if (recvLen == 0)
+			{
+				// 연결 끊김
+				break;
+			}
+
+			cout << "Recv Data Len = " << recvLen << endl;
+			break;
+
+		}
+
+		this_thread::sleep_for(1s);
+
+	}
 	
 	// 소켓 리소스 반환
 	::closesocket(clientSocket);
--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -31,35 +31,92 @@ int main()
 		return 0;
 	}
 
-	// 옵션을 해석하고 처리할 주체?
-	// 소켓 코드 -> SOL_SOCKET
-	// Ipv4 -> IPPROTO_IP
-	// TCP 프로토콜 -> IPPROTO_TCP
-	
-	// SO_KEEPALIVE = 주기적으로 연결 상태 확인 여부 (TCP only)
-	// 상대방이 소리소문없이 연결 끊는다면?
-	// 주기적으로 TCP 프로토콜 연결 상태 확인 -> 끊어진 연결 감지
-	bool enable = true;
-	::setsockopt(serverSocket, SOL_SOCKET, SO_KEEPALIVE, (char*)&enable, sizeof(enable));
+	// 블로킹(Blocking) 소캣
+	// accept -> 접속한 클라가 있을때
+	// connect -> 서버 접속 성공했을때
+	// send, sendto -> 요청한 데이터를 송신 버퍼에 복사했을때
+	// recv, recvfrom -> 수신 버퍼에 도착한 데이터가 있고, 이를 유저레벨 버퍼에 복사했을때
 
-	// SO_LINGER = 지연하다
-	// 송신 버퍼에 있는 데이터를 보낼 것인가? 날릴 것인가?
-	// onoff = 0 이면 closesocket()이 바로 리턴, 아니면 linger초만큼 대기(default 0)
-	// linger : 대기 시간
-	LINGER linger;
-	linger.l_onoff = 1;
-	linger.l_linger = 5;
-	::setsockopt(serverSocket, SOL_SOCKET, SO_LINGER, (char*)&linger, sizeof(linger));
+	// 논블로킹(Non-Blocking)
+	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSocket == INVALID_SOCKET)
+		return 0;
+	u_long on = 1;
+	::ioctlsocket(listenSocket, FIONBIO, &on);
 
-	// Half-Close
-	// SD_SEND : send 막는다
-	// SD_RECEIVE : recv 막는다
-	// SD_BOTH : 둘다 막는다
-	::shutdown(serverSocket, SD_SEND);
+	SOCKADDR_IN serverAddr;
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);
+	serverAddr.sin_port = ::htons(7777);
+
+	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+		return 0;
+
+	if (::listen(listenSocket, SOMAXCONN) == SOCKET_ERROR)
+		return 0;
+
+	cout << "Accept" << endl;
+
+	SOCKADDR_IN clientAddr;
+	int32 addrLen = sizeof(clientAddr);
+
+	while (true)
+	{
+		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+		if (clientSocket == INVALID_SOCKET)
+		{
+			// 원래 블록했어야했는데,,, 너가 논블로킹으로 하라며??
+			if (::WSAGetLastError() == WSAEWOULDBLOCK)
+				continue;
+
+			//Error
+			break;
+		}
+
+		cout << "Client Connected" << endl;
+
+		// Recv
+		while (true)
+		{
+			char recvBuffer[1000];
+			int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
+			if (recvLen == SOCKET_ERROR)
+			{
+				// 원래 블록했어야했는데,,, 너가 논블로킹으로 하라며??
+				if (::WSAGetLastError() == WSAEWOULDBLOCK)
+					continue;
+
+				// Error
+				break;
+			}
+			else if (recvLen == 0)
+			{
+				// 연결 끊김
+				break;
+			}
+
+			cout << "Recv Data Len = " << recvLen << endl;
+
+			//Send
+			while (true)
+			{
+				if (::send(clientSocket, recvBuffer, recvLen, 0) == SOCKET_ERROR)
+				{
+					// 원래 블록했어야했는데,,, 너가 논블로킹으로 하라며??
+					if (::WSAGetLastError() == WSAEWOULDBLOCK)
+						continue;
+
+					// Error
+					break;
+				}
+
+				cout << "Send Data! Len = " << recvLen << endl;
+			}
+		}
+	}
 	
-	// 소켓 리소스 반환
-	// send -> close socket
-	::closesocket(serverSocket);
+	
 
 	// 윈속 종료
 	::WSACleanup();
