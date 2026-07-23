342861adc5f84d4628ac7293ad103e70d95daa69
jej10e5 <jej10e5@gmail.com>
2026-07-23 11:13:05 +0900

    SendBuffer Chunk 및 관리 매니저 생성

--- ServerCore/SendBuffer.cpp
+++ ServerCore/SendBuffer.cpp
@@ -3,18 +3,117 @@
 /* --------------
 	SendBuffer
 --------------*/
-SendBuffer::SendBuffer(int32 bufferSize)
+SendBuffer::SendBuffer(SendBufferChunkRef owner, BYTE* buffer, int32 allocSize)
+	: _owner(owner), _buffer(buffer), _allocSize(allocSize)
 {
-	_buffer.resize(bufferSize);
+	
 }
 
 SendBuffer::~SendBuffer()
 {
 }
 
-void SendBuffer::CopyData(void* data, int32 len)
+void SendBuffer::Close(uint32 writeSize)
 {
-	ASSERT_CRASH(Capacity() >= len);
-	::memcpy(_buffer.data(), data, len);
-	_writeSize = len;
+	ASSERT_CRASH(_allocSize >= writeSize);
+	_writeSize = writeSize;
+	_owner->Close(_writeSize);
 }
+
+/* ---------------------
+	SendBufferChunk
+---------------------*/
+
+SendBufferChunk::SendBufferChunk()
+{
+}
+
+SendBufferChunk::~SendBufferChunk()
+{
+}
+
+void SendBufferChunk::Reset()
+{ // 청크를 처음부터 다시 사용하도록 위치 상태 초기화
+	_open = false;
+	_usedSize = 0;
+}
+
+SendBufferRef SendBufferChunk::Open(uint32 allocSize)
+{
+	// 어차피 tls 영역이라서 싱글스레드처럼 생각해도 상관 없음
+	ASSERT_CRASH(allocSize <= SEND_BUFFER_CHUNK_SIZE);
+	ASSERT_CRASH(_open == false);
+
+	if (allocSize > FreeSize())
+		return nullptr;
+
+	_open = true;
+	return ObjectPool<SendBuffer>::MakeShared(shared_from_this(), Buffer(), allocSize);
+}
+
+void SendBufferChunk::Close(uint32 writeSize)
+{
+	ASSERT_CRASH(_open == true);
+	_open = false;
+	_usedSize += writeSize;
+}
+
+
+
+
+/* ---------------------
+	SendBufferManager
+---------------------*/
+
+SendBufferRef SendBufferManager::Open(uint32 size)
+{
+	// 큰 메모리를 할당 받아서 앞에서부터 쭉 쓰다가
+	// 다 사용을 했으면 할당 받았던 버퍼 청크를 다 날리고
+	// 새로 청크를 할당 받는다.
+
+	if (LSendBufferChunk == nullptr)
+	{
+		LSendBufferChunk = Pop(); // WRITE_LOCK
+		LSendBufferChunk->Reset();
+	}
+
+	ASSERT_CRASH(LSendBufferChunk->IsOpen() == false);
+
+	// 다 썼으면 버리고 새거로 교체
+	if (LSendBufferChunk->FreeSize() < size)
+	{
+		LSendBufferChunk = Pop(); // WRITE_LOCK
+		LSendBufferChunk->Reset();
+	}
+
+	cout << "FREE : " << LSendBufferChunk->FreeSize() << endl;
+
+	return LSendBufferChunk->Open(size);
+}
+
+SendBufferChunkRef SendBufferManager::Pop()
+{
+	{
+		WRITE_LOCK;
+		if (_sendBufferChunks.empty() == false)
+		{
+			SendBufferChunkRef sendBufferChunk = _sendBufferChunks.back();
+			_sendBufferChunks.pop_back();
+			return sendBufferChunk;
+		}
+
+	}
+	return SendBufferChunkRef(xnew<SendBufferChunk>(), PushGlobal);
+}
+
+void SendBufferManager::Push(SendBufferChunkRef buffer)
+{
+	WRITE_LOCK;
+	_sendBufferChunks.push_back(buffer);
+}
+
+void SendBufferManager::PushGlobal(SendBufferChunk* buffer)
+{
+	GSendBufferManager->Push(SendBufferChunkRef(buffer, PushGlobal));
+}
+
